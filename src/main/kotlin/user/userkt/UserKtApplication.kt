ff134d017aabc76f345092f93d7aa3edbdb78b04
Rafael Jimenez <ext_randjimenezm@falabella.cl>
2023-02-01 19:48:06 -0500

    Kotlin base code

---
+++ src/main/kotlin/user/userkt/UserKtApplication.kt
@@ -0,0 +1,11 @@
+package user.userkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserKtApplication>(*args)
+}
