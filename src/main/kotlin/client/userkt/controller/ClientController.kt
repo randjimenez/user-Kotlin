dfd83593e8876f6f58bc5255dac1c5b8a7063a36
Rafael Jimenez <ext_randjimenezm@falabella.cl>
2023-02-02 20:03:22 -0500

    [user_002] Change controller response

--- src/main/kotlin/client/userkt/controller/ClientController.kt
+++ src/main/kotlin/client/userkt/controller/ClientController.kt
@@ -1,16 +1,18 @@
 package client.userkt.controller
 
-import client.userkt.data.entity.ClientEntity
+import client.userkt.data.dto.Client
+import client.userkt.mapper.ClientMapper
 import client.userkt.service.ClientService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @ControllerAdvice
-class ClientController(private val clientService: ClientService) {
+class ClientController(private val clientService: ClientService,
+    private val clientMapper: ClientMapper ) {
 
     @GetMapping("find")
-    fun findUserByDni(@RequestParam dni: Long): ResponseEntity<ClientEntity> {
-        return ResponseEntity.ok().body(clientService.findUser(dni))
+    fun findUserByDni(@RequestParam dni: Long): ResponseEntity<Client> {
+        return ResponseEntity.ok().body(clientMapper.toDto(clientService.findUser(dni)))
     }
 }
