8365e2c6910b7d03e0eae82285f3a8bf2c7845d4
Rafael Jimenez <ext_randjimenezm@falabella.cl>
2023-02-10 15:12:06 -0500

    [user_004] Add update user feature

--- src/main/kotlin/client/userkt/service/ClientService.kt
+++ src/main/kotlin/client/userkt/service/ClientService.kt
@@ -5,4 +5,5 @@ import client.userkt.data.entity.ClientEntity
 interface ClientService {
     fun findUser(dni: Long): ClientEntity
     fun createClient(client: ClientEntity): ClientEntity
+    fun updateClient(client: ClientEntity): ClientEntity
 }
